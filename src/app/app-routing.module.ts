97162ea4cee614809270030678387754d36e31fa
Riteh1194 <yadavritesh1194@gmail.com>
2020-01-27 11:36:53 +0530

    [Ritesh Yadav].add Updated app-routing.model.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotpasswordComponent } from "./forgotpassword/forgotpassword.component";
+import { ResetpasswordComponent } from "./resetpassword/resetpassword.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: "login", component: LoginComponent },
+  { path: '', component: LoginComponent },
+  { path: "register", component: RegistrationComponent },
+  { path: "forgotpassword", component: ForgotpasswordComponent },
+  { path: "resetpassword/:token", component: ResetpasswordComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
