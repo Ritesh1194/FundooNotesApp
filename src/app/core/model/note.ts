8e174195c7c2b29bb0c7668b8c6dd2fe1e11923e
Riteh1194 <yadavritesh1194@gmail.com>
2020-02-13 18:29:13 +0530

    [Ritesh Yadav].add Updated Model Folder

---
+++ src/app/core/model/note.ts
@@ -0,0 +1,14 @@
+export class Note {
+   noteId:number;
+   user_id:number;
+   title:string;
+   description:string;
+   isArchieve:boolean;
+   isTrashed:boolean;
+   isPinned:boolean;
+   color:string;
+   remainder:string;
+  id: any;
+}
+
+
