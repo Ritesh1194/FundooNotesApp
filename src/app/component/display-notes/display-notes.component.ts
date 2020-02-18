bfd59ffb3dee4de9d1c7a51fe82fb1fb65fdf75c
Riteh1194 <yadavritesh1194@gmail.com>
2020-02-18 19:30:20 +0530

    [Ritesh Yadav].add Updated display-notes.component.ts In Display Notes Component In FundooNotesApp

--- src/app/component/display-notes/display-notes.component.ts
+++ src/app/component/display-notes/display-notes.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { FormGroup, FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { NoteServiceService } from 'src/app/core/services/note-service.service';
 import { MatDialog } from '@angular/material/dialog';
-import { EditComponent } from '../edit/edit.component';
-
+import { UpdatenoteComponent } from 'src/app/component/updatenote/updatenote.component';
+import { CreatenoteComponent } from '../createnote/createnote.component';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-display-notes',
   templateUrl: './display-notes.component.html',
@@ -13,31 +14,27 @@ import { EditComponent } from '../edit/edit.component';
 })
 export class DisplayNotesComponent implements OnInit {
 
-  notes:[];
-  getAllNotes:[];
-  constructor(private noteservice:NoteServiceService,public dialog: MatDialog) { }
+  notes: any[];
+  getAllNotes: [];
+  constructor(private noteservice: NoteServiceService, public dialog: MatDialog) { }
 
   ngOnInit() {
-    this.noteservice.getAllNotes(localStorage.getItem('token')).subscribe((response:any)=>
-    {
+    this.noteservice.getAllNotes(localStorage.getItem('token')).subscribe((response: any) => {
       console.log(response);
-      
-      this.notes=response.object;
-         
-    })
-    
-    
+      this.notes = response.obj;
+    });
+
   }
-  openDialog(note: any)
-  {
-    console.log("open"+note.id);
-    
-    const dialogref=this.dialog.open(EditComponent, {
-      panelClass: 'custom-dialog-container' ,
-      width:'600px',
-      data:{note}
+  openDialog(note: any) {
+    console.log("open" + this.notes);
+
+    const dialogRef = this.dialog.open(UpdatenoteComponent, {
+      panelClass: 'custom-dialog-container',
+      width: '600px',
+      data: { note }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
     });
   }
-
-
 }
