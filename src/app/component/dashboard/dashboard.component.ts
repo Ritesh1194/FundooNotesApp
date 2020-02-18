a1b031c45be155a4951834a13d6a8e778717a631
Riteh1194 <yadavritesh1194@gmail.com>
2020-02-18 19:28:45 +0530

    [Ritesh Yadav].add Updated dashboard.component.ts In Dashboard Component In FundooNotesApp

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,10 +6,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
-    
   }
-  
 }
