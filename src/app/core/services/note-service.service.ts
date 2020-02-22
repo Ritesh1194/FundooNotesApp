22d9f0404a29f337eaf423f65d15a015a5abeb6d
Riteh1194 <yadavritesh1194@gmail.com>
2020-02-22 18:59:56 +0530

    [Ritesh Yadav].add Updated note-service.service.ts  In Services Package In FundooNotesApp

--- src/app/core/services/note-service.service.ts
+++ src/app/core/services/note-service.service.ts
@@ -1,18 +1,51 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, Input } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject, Subscription, Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Note } from '../model/note';
+import { MAT_DIALOG_DATA } from '@angular/material';
+import { Router, NavigationStart } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
+@Injectable()
 export class NoteServiceService {
+  subcribe: any;
+  private subject = new Subject<Note>();
+  private keepAfterRouteChange = false;
 
+  private noteId;
+  private content = new BehaviorSubject<number>(0);
+  public share = this.content.asObservable();
   private httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
-  constructor(private http: HttpClient) { }
+
+  private updateSubscription: Subscription;
+
+  constructor(private router: Router, private http: HttpClient, @Inject(MAT_DIALOG_DATA) public note: Note) {
+    // router.events.subscribe(event => {
+    //   if (event instanceof NavigationStart) {
+    //     if (this.keepAfterRouteChange) {
+    //       this.keepAfterRouteChange = false;
+
+    //     } else {
+    //       this.clear();
+    //     }
+    //   }
+    // })
+  }
+  // clear(remainder?: string) {
+  //   this.subject.next(new Note({ remainder }));
+  // }
+  // alertNote(remainder: Note) {
+  //   this.keepAfterRouteChange = remainder.keepAfterRouteChange;
+  //   this.subject.next(remainder);
+  // }
+
   createNote(Note: any, token: string): Observable<any> {
-    return this.http.post<any>(`${environment.notesApiUrl}/${environment.notesCreateUrl}/${localStorage.token}`, Note, { headers: new HttpHeaders().set('token', token) });
+    console.log(`${environment.notesApiUrl}/${environment.notesCreateUrl}`, Note, { headers: new HttpHeaders().set('token', token) });
+    return this.http.post<any>(`${environment.notesApiUrl}/${environment.notesCreateUrl}`, Note, { headers: new HttpHeaders().set('token', token) });
   }
 
   getAllNotes(token: string): Observable<any> {
@@ -22,7 +55,7 @@ export class NoteServiceService {
   updateNote(Note: any, token: string, noteId: Number): Observable<any> {
     console.log(noteId);
 
-    return this.http.post<any>(`${environment.notesApiUrl}/${environment.notesUpdateUrl}/${localStorage.token}`, Note, { headers: new HttpHeaders().set('token', token) })
+    return this.http.post<any>(`${environment.notesApiUrl}/${environment.notesUpdateUrl}`, Note, { headers: new HttpHeaders().set('token', localStorage.token) })
 
   }
 
@@ -30,39 +63,30 @@ export class NoteServiceService {
     return this.http.delete<any>(`${environment.notesApiUrl}${environment.notesDeleteUrl}`, { headers: new HttpHeaders().set('token', token) })
   }
 
-  // getTrashedNotes(token: string): Observable<any> {
-  //   return this.http.get<any>(`${environment.notesApiUrl}${environment.getTrashedUrl}`, { headers: new HttpHeaders().set('token', token) })
-  // }
+  reminderNotes(noteId: number, token: string, Note: any) {
+    return this.http.post<any>(`${environment.notesApiUrl}${environment.addReminderUrl}?noteId=${noteId}`, Note, { headers: new HttpHeaders().set('token', token) })
 
-  // getArchivedNotes(token: string): Observable<any> {
-  //   return this.http.get<any>(this.API_URL + "archievednotes", { headers: new HttpHeaders().set('token', token) })
-  // }
+  }
+  pinNotes(noteId: number): Observable<any> {
+    console.log(`${environment.notesApiUrl}/${environment.pinNotesUrl}?noteId=${noteId}`);
+    return this.http.post<any>(`${environment.notesApiUrl}/${environment.pinNotesUrl}?noteId=${noteId}`, Note, { headers: new HttpHeaders().set('token', localStorage.token) })
+  }
+  getAllPinnedNotes(token: string): Observable<any> {
+    console.log(`${environment.notesApiUrl}/${environment.pinNotesUrl}`);
+    return this.http.post<any>(`${environment.notesApiUrl}/${environment.getAllPinnedNotesUrl}`, Note, { headers: new HttpHeaders().set('token', localStorage.token) })
+  }
 
-  // pinNotes(token: string, id: any): Observable<any> {
-
-  //   return this.http.post<any>(this.API_URL + "notes/pinned/" + id, { headers: new HttpHeaders().set('token', token) })
-
-  // }
-  // unPinNotes(token: string, id: any): Observable<any> {
-
-  //   return this.http.post<any>(this.API_URL + "notes/unpinned/" + id, { headers: new HttpHeaders().set('token', token) })
-  // }
-
-  // archieveNote(token: string, id: any): Observable<any> {
-  //   return this.http.post<any>(this.API_URL + "notes/archieve/" + id, { headers: new HttpHeaders().set('token', token) })
-  // }
-
-  // unarchieveNote(token: string, id: any): Observable<any> {
-  //   return this.http.post<any>(this.API_URL + "notes/unarchieve/" + id, { headers: new HttpHeaders().set('token', token) })
-  // }
+  archieveNote(token: string, id: any): Observable<any> {
+    return this.http.post<any>(`${environment.notesApiUrl}${environment.archiveUrl}/${localStorage.token}`, { headers: new HttpHeaders().set('token', token) })
+  }
 
   trashnote(token: string, id: any): Observable<any> {
     return this.http.get<any>(`${environment.notesApiUrl}${environment.getTrashedUrl}`, { headers: new HttpHeaders().set('token', token) })
   }
-
-  // restoreNote(token: string, id: any): Observable<any> {
-  //   return this.http.post<any>(this.API_URL + "notes/restore/" + id, { headers: new HttpHeaders().set('token', token) })
-  // }
-
-
+  addColor(noteId: Number, Note: any, token: string): Observable<any> {
+    return this.http.post<any>(`${environment.notesApiUrl}${environment.addColorUrl}/${localStorage.getItem('token')}`, { headers: new HttpHeaders().set('token', localStorage.token) })
+  }
+  updateNotes(noteId) {
+    this.content.next(noteId);
+  }
 }
