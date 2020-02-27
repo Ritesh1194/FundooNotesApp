2a60d1e3fd23e539979d11ea9b17358485824091
Riteh1194 <yadavritesh1194@gmail.com>
2020-02-27 19:17:29 +0530

    [Ritesh Yadav].add Updated iconlist.component.ts Component In FundooNotesApp

--- src/app/component/iconlist/iconlist.component.ts
+++ src/app/component/iconlist/iconlist.component.ts
@@ -1,82 +1,72 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Router, OutletContext } from '@angular/router';
 import { NoteServiceService } from 'src/app/core/services/note-service.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Note } from 'src/app/core/model/note';
 import { Subscriber } from 'rxjs';
+import { MatDialogRef } from '@angular/material';
+import { DisplayNotesComponent } from '../display-notes/display-notes.component';
+
+
 @Component({
   selector: 'app-iconlist',
   templateUrl: './iconlist.component.html',
   styleUrls: ['./iconlist.component.scss']
 })
 export class IconlistComponent implements OnInit {
-  @Input() noteData: any;
+  @Input() xyz: Note;
   notes: Note = new Note();
-  note: any;
-  dialogRef: any;
 
+  dialogRef: any;
+  AllArchiveNotes: any;
   constructor(private router: Router, private noteservice: NoteServiceService, private snackbar: MatSnackBar) { }
   Token = localStorage.getItem('token')
   ngOnInit() {
   }
 
-
-  getNoteId(items: any) {
-    this.notes.noteId = items.noteId;
-  }
-  delete() {
-    this.noteservice.deleteNote(this.Token, this.notes.noteId).subscribe((note) => {
+  deleteNote() {
+    console.log("noteId--->" + this.xyz.noteId);
+    this.noteservice.deleteNote(this.xyz.noteId, localStorage.getItem('token')).subscribe(response => {
       this.snackbar.open('Note deleted successfully', 'Ok', { duration: 3000 });
 
     },
       (error: any) => {
-        console.log("hello" + error);
-        this.snackbar.open(error.error.description, 'error', { duration: 3000 });
+        this.snackbar.open(error, 'error', { duration: 3000 });
       });
+  }
 
-  }
-  saveRemainder(selectedMoment: any, notes) {
-    notes.remainder = selectedMoment;
-    this.SetRemainder();
-  }
-  SetRemainder() {
-    console.log(this.notes.noteId);
-    this.noteservice.reminderNotes(this.notes.noteId, this.Token, this.notes.remainder).subscribe((notes) => {
+  saveRemainder(selectedMoment: any) {
+    this.xyz.remainder = selectedMoment;
+
+    console.log(this.xyz.noteId);
+    this.noteservice.reminderNotes(this.xyz.noteId, localStorage.Token, this.xyz.remainder).subscribe((response) => {
       this.snackbar.open('Reminder Set successfully', 'Ok', { duration: 3000 });
     },
       (error: any) => {
-        console.log(error);
-        this.snackbar.open(error.error, 'error', { duration: 3000 });
+        this.snackbar.open(error, 'error', { duration: 3000 });
       });
   }
 
-  // updateArchiveNote(key, note) {
-  //   note.isArchieve = key === 'isArchieve' ? 1 : 0;
-  //   note.isArchieve = 0;
-  //   this.updateArchive();
-  // }
-  // updateArchive() {
-  //   this.noteservice.archieveNote(localStorage.getItem('token'), this.notes.noteId
-  //   ).subscribe(response => {
-  //     console.log(response);
-  //     this.dialogRef.close();
-  //   },
-  //     error => {
-  //       console.log("error");
-  //     })
-  // }
-  // token(newNote: any, noteId: any, token: any) {
-  //   throw new Error("Method not implemented.");
-  // }
-
   changeColor() {
-    this.noteservice.addColor(this.notes.noteId, this.notes.color, localStorage.getItem('token')).subscribe((note) => {
+    this.noteservice.addColor(this.xyz.noteId, this.xyz.color, localStorage.getItem('token')).subscribe((note) => {
       this.snackbar.open('Color Updated successfully', 'Ok', { duration: 3000 });
 
     },
       (error: any) => {
-        console.log("hello" + error);
-        this.snackbar.open(error.error, 'error', { duration: 3000 });
+        this.snackbar.open(error, 'error', { duration: 3000 });
       });
+  }
+
+  onClickNoteId(noteId, isArchive) {
+    this.noteservice.updateNotes(noteId);
+  }
+
+  archiveNotes() {
+    this.noteservice.archieveNotes(this.notes.noteId, localStorage.getItem('token')).subscribe(
+      (data) => {
+        console.log(data.obj);
+        this.AllArchiveNotes = data.obj;
+      }
+    );
   }
 }
