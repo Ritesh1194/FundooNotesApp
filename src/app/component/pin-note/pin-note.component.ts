767e747eeed9a36b783dde812b2ac9134311e80e
Riteh1194 <yadavritesh1194@gmail.com>
2020-02-27 19:18:14 +0530

    [Ritesh Yadav].add Updated pin-note.component.ts Component In FundooNotesApp

--- src/app/component/pin-note/pin-note.component.ts
+++ src/app/component/pin-note/pin-note.component.ts
@@ -6,7 +6,8 @@ import { NoteServiceService } from 'src/app/core/services/note-service.service';
 import { FormControl } from '@angular/forms';
 import { Label } from 'src/app/core/model/label';
 import { DomSanitizer } from '@angular/platform-browser';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { NotesService } from 'src/app/core/services/notes.service';
 
 @Component({
   selector: 'app-pin-note',
@@ -14,35 +15,42 @@ import { Observable } from 'rxjs';
   styleUrls: ['./pin-note.component.scss']
 })
 export class PinNoteComponent implements OnInit {
-  [x: string]: any;
-
-  note: Note = new Note();
+  @Input() xyz: Note;
   isPinned: boolean;
-  constructor(@Inject(MAT_DIALOG_DATA) public notes: Note, private noteService: NoteServiceService, private snackBar: MatSnackBarModule,
-    public dialog: MatDialog, private sanitizer: DomSanitizer, private matSnackBar: MatSnackBar) { }
+  pinnotes: any;
+  notes: Note[];
+  subscription: Subscription;
+  constructor(@Inject(MAT_DIALOG_DATA) private snackBar: MatSnackBarModule,
+    public dialog: MatDialog, private matSnackBar: MatSnackBar, private noteService: NoteServiceService, private notesService: NotesService) {
+
+  }
 
   ngOnInit() {
   }
 
   pinnedNote() {
-    console.log(this.note.noteId);
-    this.noteService.share.subscribe(value => this.note.noteId = value);
-    this.noteService.pinNotes(this.note.noteId).subscribe(response => {
+    console.log(this.xyz.noteId);
+    this.noteService.pinNotes(this.xyz.noteId).subscribe(response => {
       if (this.isPinned) {
-        this.isPinned = true;
-        this.matSnackBar.open("Note Pinned Successfully", 'Ok', { duration: 5000 });
-        // this.dialogRef.close();
+        this.isPinned = false;
+        this.matSnackBar.open("Note UnPinned Successfully", 'Ok', { duration: 5000 });
+
       }
       else if (!this.isPinned) {
-        this.isPinned = false;
-        this.matSnackBar.open("Note unPinned Successfully", 'Ok', { duration: 5000 });
-        // this.dialogRef.close();
+        this.isPinned = true;
+        this.matSnackBar.open("Note Pinned Successfully", 'Ok', { duration: 5000 });
+
       }
       console.log(response);
-      // this.dialogRef.close();
+
     },
       (error: any) => {
         console.log("error");
       });
+    this.noteService.getAllPinnedNotes(localStorage.getItem('token')).subscribe((response: any) => {
+      this.pinnotes = response.obj;
+      console.log("Pin Notes ARe ", this.pinnotes);
+
+    });
   }
 }
