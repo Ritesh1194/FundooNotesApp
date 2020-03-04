a6bef1956ed9946b7aa039e9841bb8be61a9906c
Riteh1194 <yadavritesh1194@gmail.com>
2020-03-04 19:00:43 +0530

    [Ritesh Yadav].add Updated iconlist.component.ts Component In FundooNotesApp

--- src/app/component/iconlist/iconlist.component.ts
+++ src/app/component/iconlist/iconlist.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, Inject } from '@angular/core';
 import { Router, OutletContext } from '@angular/router';
 import { NoteServiceService } from 'src/app/core/services/note-service.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Note } from 'src/app/core/model/note';
 import { Subscriber } from 'rxjs';
-import { MatDialogRef } from '@angular/material';
+import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material';
 import { DisplayNotesComponent } from '../display-notes/display-notes.component';
+import { LabelserviceService } from 'src/app/core/services/labelservice.service';
+import { Label } from 'src/app/core/model/label';
+import { UpdatenoteComponent } from '../updatenote/updatenote.component';
+import { CollaboratorComponent } from '../collaborator/collaborator.component';
 
 
 @Component({
@@ -15,58 +19,122 @@ import { DisplayNotesComponent } from '../display-notes/display-notes.component'
 })
 export class IconlistComponent implements OnInit {
   @Input() xyz: Note;
+  @Input() label: Label;
+  noteId: number;
+  isArchive: boolean = false;
   notes: Note = new Note();
+  archiveNote: any;
+  @Input() note
+  @Output() eventAddNoteLabel = new EventEmitter();
+  // public labels: Label[] = [];
+  labels: Label = new Label();
+  public newLabels: Label[] = [];
+  public filter = '';
 
-  dialogRef: any;
-  AllArchiveNotes: any;
-  constructor(private router: Router, private noteservice: NoteServiceService, private snackbar: MatSnackBar) { }
+  constructor(private router: Router, private noteservice: NoteServiceService, private labelService: LabelserviceService,
+    private snackBar: MatSnackBar, public dialog: MatDialog,
+    public dialogRef: MatDialogRef<DisplayNotesComponent>,
+
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
   Token = localStorage.getItem('token')
   ngOnInit() {
   }
+  saveRemainder(reminder: any) {
+    this.xyz.reminder = reminder;
+    console.log(this.xyz.reminder);
+    this.noteservice.reminderNotes(this.xyz.noteId, reminder).subscribe((response) => {
 
-  deleteNote() {
-    console.log("noteId--->" + this.xyz.noteId);
-    this.noteservice.deleteNote(this.xyz.noteId, localStorage.getItem('token')).subscribe(response => {
-      this.snackbar.open('Note deleted successfully', 'Ok', { duration: 3000 });
-
+      this.snackBar.open('Reminder Set successfully', 'Ok', { duration: 3000 });
     },
       (error: any) => {
-        this.snackbar.open(error, 'error', { duration: 3000 });
+        this.snackBar.open(error, 'error', { duration: 3000 });
       });
   }
 
-  saveRemainder(selectedMoment: any) {
-    this.xyz.remainder = selectedMoment;
-
-    console.log(this.xyz.noteId);
-    this.noteservice.reminderNotes(this.xyz.noteId, localStorage.Token, this.xyz.remainder).subscribe((response) => {
-      this.snackbar.open('Reminder Set successfully', 'Ok', { duration: 3000 });
-    },
-      (error: any) => {
-        this.snackbar.open(error, 'error', { duration: 3000 });
-      });
-  }
-
-  changeColor() {
-    this.noteservice.addColor(this.xyz.noteId, this.xyz.color, localStorage.getItem('token')).subscribe((note) => {
-      this.snackbar.open('Color Updated successfully', 'Ok', { duration: 3000 });
-
-    },
-      (error: any) => {
-        this.snackbar.open(error, 'error', { duration: 3000 });
-      });
-  }
 
   onClickNoteId(noteId, isArchive) {
     this.noteservice.updateNotes(noteId);
   }
 
-  archiveNotes() {
-    this.noteservice.archieveNotes(this.notes.noteId, localStorage.getItem('token')).subscribe(
-      (data) => {
-        console.log(data.obj);
-        this.AllArchiveNotes = data.obj;
+  onClickDelete() {
+    // this.noteId = this.xyz.noteId
+    this.noteservice.deleteNote(this.xyz.noteId).subscribe((response) => {
+      this.snackBar.open("Note unpinned and trashed", 'ok', { duration: 5000 });
+    },
+      error => {
+        this.snackBar.open("error in Note Deletion", 'ok', { duration: 5000 });
       }
+
     );
+  }
+
+  onClickArchive() {
+    this.noteservice.archieveNotes(this.xyz.noteId).subscribe((response) => {
+
+      this.snackBar.open("Note unpinned and Archived", "OK", { duration: 5000 });
+    },
+      error => {
+        this.snackBar.open("error in Note thrash operation", "OK", { duration: 5000 });
+      });
+  }
+
+  setColor(color) {
+    console.log("Color---->", this.xyz.color, this.xyz.noteId);
+
+    this.noteservice.addColor(this.xyz.noteId, color).subscribe(res => {
+
+      console.log("Resssponse backk---->");
+
+      console.log("Response after setting note color-------->", res.obj);
+    });
+  }
+
+  arrayOfColors = [
+    [
+      { color: "rgb(255, 179, 255)", name: "pink" },
+      { color: "rgb(255, 255, 128)", name: "darkGolden" },
+      { color: "white", name: "white" }
+    ],
+    [
+      { color: "slategray", name: "grey" },
+      { color: "rgb(153, 221, 255)", name: "light blue" },
+      { color: "rgb(200, 232, 104)", name: "yellow" }
+    ],
+    [
+      { color: "rgb(255, 153, 0)", name: "orange" },
+      { color: "rgb(97, 191, 82)", name: "green" },
+      { color: " rgb(158, 136, 191)", name: "darkYellow" }
+    ]
+  ]
+
+  public createNewLabel(note) {
+    this.labelService.createLabel(this.labels.name).subscribe(response => {
+      console.log("adding check in database");
+      const data = { note };
+      this.eventAddNoteLabel.emit(data);
+      this.snackBar.open("label created", "Ok", { duration: 2000 });
+    }, error => {
+      this.snackBar.open("error", "error to create labels", { duration: 2000 });
+    }
+    )
+  }
+  public onClickCheckbox(event, label, note) {
+    event.stopPropagation();
+    this.labelService.createLabel(this.labels.name).subscribe(response => {
+      console.log("adding check in database");
+      const data = { note };
+      this.eventAddNoteLabel.emit(data);
+    }, (error) => console.log(error));
+  }
+
+
+  public dailogCollaborator(note) {
+    const dialogRef = this.dialog.open(CollaboratorComponent, {
+      width: '500px',
+      data: note
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 }
