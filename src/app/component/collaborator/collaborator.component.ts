f101ff06fffd3b4dc82ed513b409d023e34987e7
Riteh1194 <yadavritesh1194@gmail.com>
2020-03-04 19:08:54 +0530

    [Ritesh Yadav].add Updated collaborator.component.ts Component In FundooNotesApp

---
+++ src/app/component/collaborator/collaborator.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { UserserviceService } from 'src/app/core/services/userservice.service';
+import { User } from 'src/app/core/model/user';
+import { MatDialogRef, MAT_DIALOG_DATA, MatSnackBar } from '@angular/material';
+import { Note } from 'src/app/core/model/note';
+import { DomSanitizer } from '@angular/platform-browser';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { NoteServiceService } from 'src/app/core/services/note-service.service';
+import { error } from 'util';
+
+interface ImageData {
+  imageSrc: any;
+}
+
+@Component({
+  selector: 'app-collaborator',
+  templateUrl: './collaborator.component.html',
+  styleUrls: ['./collaborator.component.scss']
+})
+export class CollaboratorComponent implements OnInit {
+  public email = '';
+  public imageData = <ImageData>{};
+  public myControl = new FormControl();
+  public users: User[] = [];
+  public collabUsers: User[] = [];
+
+
+  constructor(private userService: UserserviceService, public dialogRef: MatDialogRef<CollaboratorComponent>,
+    @Inject(MAT_DIALOG_DATA) public note: Note,
+    private snackBar: MatSnackBar, private sanitizer: DomSanitizer,
+    private noteService: NoteServiceService) { }
+
+  ngOnInit() {
+    this.getUsers();
+    //this.getCollaborateUser();
+    // this.getNoteOwner();
+  }
+
+  // public getNoteOwner()
+  // {
+  //   this.userService.getCollaborateUser(this.note.userId).subscribe(
+  //     user => this.user=user);
+  // }
+  public getUsers() {
+    this.userService.getUsers().subscribe(({ body }) => {
+      this.users = body;
+      console.log(this.users)
+    }
+      , error => console.log("error"));
+  }
+
+  collaborate(email) {
+    this.noteService.createCollaborator(this.email, this.note.noteId).subscribe(resp => {
+      this.dialogRef.close();
+      this.snackBar.open("added to collaborator", "ok", { duration: 2000 })
+    }
+    );
+    error => {
+      this.snackBar.open("email not present or collaborator already present", "error", { duration: 2000 });
+    }
+
+  }
+
+  closeClick() {
+    this.dialogRef.close();
+  }
+
+
+  // getCollaborateUser() {
+  //   for (let i = 0; i < this.note.collaborators.length; i++) {
+  //     var k = 0;
+  //     this.userService.getCollaborateUser(this.note.collaborators[i].userId).subscribe(
+  //       user => {
+  //         this.collabUsers[k] = user;
+  //         k++;
+  //       }
+  //       , error => console.log(error))
+  //   }
+  // }
+
+
+}
