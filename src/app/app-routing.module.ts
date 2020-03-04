c038d790f07dc113c645f5728a708edeb14b19d5
Riteh1194 <yadavritesh1194@gmail.com>
2020-03-04 18:53:40 +0530

    [Ritesh Yadav].add Updated App-routing.module.ts Folder In FundooNotesApp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,6 @@ import { CreatenoteComponent } from 'src/app/component/createnote/createnote.com
 import { NotesComponent } from 'src/app/component/notes/notes.component';
 import { UpdatenoteComponent } from 'src/app/component/updatenote/updatenote.component';
 import { DisplayNotesComponent } from './component/display-notes/display-notes.component';
-import { PinNoteComponent } from "src/app/component/pin-note/pin-note.component";
 const routes: Routes = [
   { path: "login", component: LoginComponent },
 
