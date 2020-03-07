34cb6646976b202ccd59ab5036a7b381923c7dcb
Riteh1194 <yadavritesh1194@gmail.com>
2020-03-07 12:26:03 +0530

    [Ritesh Yadav]. add Updated labelservice.service.ts In Services Package In FundooNotesApp

--- src/app/core/services/labelservice.service.ts
+++ src/app/core/services/labelservice.service.ts
@@ -3,23 +3,38 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { Label } from '../model/label';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LabelserviceService {
+  private subject = new Subject<any>();
   private httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+
   };
 
   constructor(private router: Router, private http: HttpClient) { }
-  createLabel(name: any) {
-    return this.http.post<any>(`${environment.notesApiUrl}/${environment.createLabelUrl}`, { headers: new HttpHeaders().set('token', localStorage.token) });
+  setLabels(message: Label[]) {
+    console.log("List Of Lables", message)
+    this.subject.next({ label: message });
   }
-  createAndMapLabel(name: any, noteId: any) {
-    return this.http.post<any>(`${environment.notesApiUrl}/${environment.createAndMapUrl}?noteId=${noteId}`, { headers: new HttpHeaders().set('token', localStorage.token) });
+
+  getLabels(): Observable<any> {
+    return this.subject.asObservable();
   }
-  addLabelToNote(labelId: any,noteId: any) {
-    return this.http.post<any>(`${environment.notesApiUrl}/${environment.addLabelUrl}?labelId=${labelId}&noteId=${noteId}`, { headers: new HttpHeaders().set('token', localStorage.token) });
+  createLabel(name: any, token: string): Observable<any> {
+    console.log(`${environment.labelApiUrl}/${environment.createLabelUrl}`);
+    return this.http.post<any>(`${environment.labelApiUrl}/${environment.createLabelUrl}`, name, { headers: new HttpHeaders().set('token', localStorage.token) });
+  }
+  createAndMapLabel(name: any, noteId: any): Observable<any> {
+    return this.http.post<any>(`${environment.labelApiUrl}/${environment.createAndMapUrl}?noteId=${noteId}`, { headers: new HttpHeaders().set('token', localStorage.token) });
+  }
+  addLabelToNote(labelId: any, noteId: any): Observable<any> {
+    return this.http.post<any>(`${environment.labelApiUrl}/${environment.addLabelUrl}?labelId=${labelId}&noteId=${noteId}`, { headers: new HttpHeaders().set('token', localStorage.token) });
+  }
+  getAllLabel() {
+    return this.http.get<any>(`${environment.labelApiUrl}/${environment.getAllLabelUrl}`, { headers: new HttpHeaders().set('token', localStorage.token) });
   }
 }
