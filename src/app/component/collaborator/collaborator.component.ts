45d25fda934514664b9ead9f35eaf50850315fa0
Riteh1194 <yadavritesh1194@gmail.com>
2020-03-14 16:21:50 +0530

    [Ritesh Yadav].add Updated collaborator.component.ts Component In FundooNotesApp

--- src/app/component/collaborator/collaborator.component.ts
+++ src/app/component/collaborator/collaborator.component.ts
@@ -20,11 +20,13 @@ interface ImageData {
   styleUrls: ['./collaborator.component.scss']
 })
 export class CollaboratorComponent implements OnInit {
+
   public email = '';
   public imageData = <ImageData>{};
   public myControl = new FormControl();
   public users: User[] = [];
   public collabUsers: User[] = [];
+  user: User = new User();
   subscription: any;
 
 
@@ -32,34 +34,32 @@ export class CollaboratorComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public note: Note,
     private snackBar: MatSnackBar, private sanitizer: DomSanitizer,
     private noteService: NoteServiceService) {
-    this.subscription = userService.getUsers().subscribe(message => {
-      console.log("in display Label subscrib....", message.users);
+    this.subscription = this.userService.getAllUsers().subscribe(message => {
       this.users = message.users;
-      console.log("Others Label:", this.users);
+      console.log(this.users);
     });
   }
 
   ngOnInit() {
     this.getUsers();
     //this.getCollaborateUser();
-    // this.getNoteOwner();
+    this.getNoteOwner();
   }
 
-  // public getNoteOwner()
-  // {
-  //   this.userService.getCollaborateUser(this.note.userId).subscribe(
-  //     user => this.user=user);
-  // }
+  public getNoteOwner() {
+    this.userService.getCollaborateUser(this.note.userId).subscribe(
+      user => this.user = user);
+  }
   public getUsers() {
-    this.userService.getUsers().subscribe(({ body }) => {
-      this.users = body;
+    this.userService.getUsers().subscribe(data => {
+      this.users = data.obj;
       console.log(this.users)
     }
       , error => console.log("error"));
   }
 
-  collaborate(email: string) {
-    this.noteService.createCollaborator(this.email, this.note.noteId).subscribe(resp => {
+  collaborate(username: string) {
+    this.noteService.createCollaborator(this.user.username, this.note.noteId).subscribe(resp => {
       //this.dialogRef.close();
       this.snackBar.open("added to collaborator", "ok", { duration: 2000 })
     }
