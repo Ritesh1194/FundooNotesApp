37b0e969051cd7149b3381d4af13334bbbc1321a
Riteh1194 <yadavritesh1194@gmail.com>
2020-03-16 18:52:10 +0530

    [Ritesh Yadav].add Updated helper-service.service.ts  In FundooNotesApp

---
+++ src/app/core/services/helper-service.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+import { Label } from 'src/app/core/model/label';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HelperServiceService {
+  public theme$: Subject<any> = new Subject();
+  // public labels$:Label[];
+  public search$: Subject<any> = new Subject();
+  public label$: Label;
+  
+  constructor() { }
+
+  public setTheme(themeChanged: boolean) {
+    this.theme$.next(themeChanged);
+  }
+
+  public getTheme() {
+    return this.theme$;
+  }
+
+  public setLabel(label:Label) {
+    this.label$=(label)
+  }
+
+  public getLabel() :Label {
+    return this.label$;
+  }
+
+  // public setLabels(labels:Label[])
+  // {
+  //   this.labels$=labels;
+  // }
+
+  // public getLabels()
+  // {
+  //   return this.labels$;
+  // }
+
+  public setSearch(search: string) {
+    this.search$.next(search);
+  }
+
+  public getSearch() {
+    return this.search$;
+  }
+}
